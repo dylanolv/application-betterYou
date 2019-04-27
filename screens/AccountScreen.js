27c3098f6cf7589968e43c63fc3c30a78b9b9dd0
dylanolv <oliveira.dylann@gmail.com>
2019-04-27 17:56:21 +0200

    Home

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AsyncStorage, View, ActivityIndicator, StyleSheet, ScrollView } from "react-native";
+import { AppState, AsyncStorage, View, ActivityIndicator, StyleSheet, ScrollView } from "react-native";
 import { Header, Container, Title, Content, List, ListItem, InputGroup, Input, Icon, Text, Button } from "native-base";
 import * as firebase from "firebase";
 
@@ -8,19 +8,33 @@ export default class AccountScreen extends React.Component {
     title: "Account"
   };
 
+  constructor(props) {
+    super(props)
+    this.state = {};
+  }
+
   componentWillMount(){
+    this.load()
+    this.props.navigation.addListener('willFocus', this.load)
+  }
+  
+  load = () => {
+    this.getUserData();
+  }
+
+  getUserData() {
     //Check if userData is stored on device else open Login
     AsyncStorage.getItem('userData').then((user_data_json) => {
       let user_data = JSON.parse(user_data_json);
+      console.log(user_data)
       if(user_data != null){
         this.props.navigation.navigate("AccountStack");
       }else{
         this.props.navigation.navigate("LoginStack");
       }
     });
-
   }
-  
+
   logout() {
     // logout, once that is complete, return the user to the login screen.
     AsyncStorage.removeItem('userData').then(() => {
@@ -28,7 +42,6 @@ export default class AccountScreen extends React.Component {
         this.props.navigation.navigate("LoginStack");
       });  
     });
-
   }
 
   render() {
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
-import { Image, StyleSheet, TouchableOpacity } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Center, Right, Body, Fab } from 'native-base';
+import { View, Image, StyleSheet, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { Container, Content, Card, CardItem, Text, Button, Icon, Left, Body } from 'native-base';
+import * as firebase from "firebase";
+
+// To hide the yellowbox
+import { YellowBox } from 'react-native';
+import _ from 'lodash';
+YellowBox.ignoreWarnings(['Setting a timer']);
+const _console = _.clone(console);
+console.warn = message => {
+  if (message.indexOf('Setting a timer') <= -1) {
+    _console.warn(message);
+  }
+};
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -10,10 +22,11 @@ export default class HomeScreen extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      active: 'false',
       upBtnSelected: false,
       downBtnSelected: false,
-      starSelected: false
+      starSelected: false,
+      loading: true, 
+      discoveries: []
     };
   }
 
@@ -29,67 +42,102 @@ export default class HomeScreen extends React.Component {
     this.setState({ downBtnSelected: !this.state.downBtnSelected, upBtnSelected: false })
   }
 
+  getDiscoveries() {
+    firebase
+      .database()
+      .ref("discoveries/")
+      .on("value", snapshot => {
+        let childData = [];
+
+        snapshot.forEach(function(childSnapshot) {
+          childData.push(childSnapshot)
+        });
+        
+        this.setState({ loading: false });
+      });
+  }
+
+  componentDidMount() {
+    this.getDiscoveries();
+  }
+
   render() {
-    return (
-      <Container>
-        <Content>
-          <Card style={{flex: 0}}>
-            <CardItem>
-              <Left>
-                <Body>
-                  <Text note>Catégorie</Text>
-                  <Text>Titre Découverte</Text>
-                </Body>
-              </Left>
-              <TouchableOpacity style={[styles.star]}>
-                <Icon style={[styles.iconStar]} name={(this.state.starSelected == true)?'star':'star-outline'} onPress={this.onPressStar.bind(this)}/>
-              </TouchableOpacity>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Image source={require('../assets/images/discovery-image1.jpg')} style={[styles.img]}/>
-                <Text style={[styles.txt]}>
-                  Nulla tempus ac urna vel pellentesque. Nullam imperdiet dictum ultricies. 
-                  Suspendisse in nisl nec orci accumsan tincidunt. Nullam at erat lacus. 
-                  In in lectus in libero ornare consequat. Phasellus tempor nisl vulputate urna ullamcorper.
-                </Text>
-                <TouchableOpacity style={[styles.more]}>
-                  <Text style={[styles.moreTxt]}>En savoir plus..</Text>
+    if (this.state.loading) {
+      return (
+        <View style={[styles.container, styles.horizontal]}>
+          <ActivityIndicator size="large" color="#67BBF2" />
+        </View>
+      )
+    }
+    else {
+      return (
+        <Container>
+          <Content>
+            <Card style={{flex: 0}}>
+              <CardItem>
+                <Left>
+                  <Body>
+                    <Text note>Minimalisme</Text>
+                    <Text style={[styles.title]}>Vivre mieux avec moins</Text>
+                  </Body>
+                </Left>
+                <TouchableOpacity style={[styles.star]}>
+                  <Icon style={[styles.iconStar]} name={(this.state.starSelected == true)?'star':'star-outline'} onPress={this.onPressStar.bind(this)}/>
                 </TouchableOpacity>
-              </Body>
-            </CardItem>
-            <CardItem style={{justifyContent: 'center'}}>
-                <Button style={[(this.state.upBtnSelected == true)?styles.btnSelected:styles.btnNotSelected, styles.marginUpDownButtons]} onPress={this.onPressUp.bind(this)}>
-                  <Icon style={(this.state.upBtnSelected == true)?styles.iconBtnSelected:styles.iconBtnNotSelected} name='trending-up'/>
-                  <Text style={(this.state.upBtnSelected == true)?styles.txtBtnSelected:styles.txtBtnNotSelected}>470</Text>
+              </CardItem>
+              <CardItem>
+                <Body>
+                  <Image source={require('../assets/images/minimalism1.jpg')} style={[styles.img]}/>
+                  <Text style={[styles.txt]}>
+                    Dans notre société où la consommation domine, un nombre croissant de personnes vont à l'encontre de ces principes et décide de posséder eet consommer moins. 
+                    Le minimalisme est une manière de se libérer de ses possessions. Nous accordons trop d'importance aux choses que nous possèdons, à tel point que nous nous sentons pièger.
+                  </Text>
+                  <TouchableOpacity style={[styles.more]}>
+                    <Text style={[styles.moreTxt]}>En savoir plus..</Text>
+                  </TouchableOpacity>
+                </Body>
+              </CardItem>
+              <CardItem style={{justifyContent: 'center'}}>
+                  <Button style={[(this.state.upBtnSelected == true)?styles.btnSelected:styles.btnNotSelected, styles.marginUpDownButtons]} onPress={this.onPressUp.bind(this)}>
+                    <Icon style={(this.state.upBtnSelected == true)?styles.iconBtnSelected:styles.iconBtnNotSelected} name='trending-up'/>
+                    <Text style={(this.state.upBtnSelected == true)?styles.txtBtnSelected:styles.txtBtnNotSelected}>470</Text>
+                  </Button>
+                  <Button style={[(this.state.downBtnSelected == true)?styles.btnSelected:styles.btnNotSelected, styles.marginUpDownButtons]} onPress={this.onPressDown.bind(this)}>
+                    <Icon style={(this.state.downBtnSelected == true)?styles.iconBtnSelected:styles.iconBtnNotSelected} name='trending-down'/>
+                    <Text style={(this.state.downBtnSelected == true)?styles.txtBtnSelected:styles.txtBtnNotSelected}>20</Text>
+                  </Button>
+              </CardItem>
+              <CardItem style={{justifyContent: 'center'}}>
+                <Button style={[styles.btnSelected, styles.marginShareCommentButtons]}>
+                  <Icon name='share' style={[styles.iconBtnSelected]}/>
+                  <Text style={[styles.txtBtnSelected]}>Partager</Text>
                 </Button>
-                <Button style={[(this.state.downBtnSelected == true)?styles.btnSelected:styles.btnNotSelected, styles.marginUpDownButtons]} onPress={this.onPressDown.bind(this)}>
-                  <Icon style={(this.state.downBtnSelected == true)?styles.iconBtnSelected:styles.iconBtnNotSelected} name='trending-down'/>
-                  <Text style={(this.state.downBtnSelected == true)?styles.txtBtnSelected:styles.txtBtnNotSelected}>20</Text>
+                <Button style={[styles.btnSelected, styles.marginShareCommentButtons]}>
+                  <Icon name='chatboxes' style={[styles.iconBtnSelected]}/>
+                  <Text style={[styles.txtBtnSelected]}>Commenter</Text>
                 </Button>
-            </CardItem>
-            <CardItem style={{justifyContent: 'center'}}>
-              <Button style={[styles.btnSelected, styles.marginShareCommentButtons]}>
-                <Icon name='share' style={[styles.iconBtnSelected]}/>
-                <Text style={[styles.txtBtnSelected]}>Partager</Text>
-              </Button>
-              <Button style={[styles.btnSelected, styles.marginShareCommentButtons]}>
-                <Icon name='chatboxes' style={[styles.iconBtnSelected]}/>
-                <Text style={[styles.txtBtnSelected]}>Commenter</Text>
-              </Button>
-            </CardItem>
-          </Card>
-        </Content>
-      </Container>
-    );
+              </CardItem>
+            </Card>
+          </Content>
+        </Container>
+      );
+    }
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center'
+  },
+  horizontal: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
+    padding: 10
+  },
+  title: {
+    fontWeight: 'bold'
   },
   more: {
     alignSelf: 'flex-end'
